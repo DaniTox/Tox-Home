d67952f5f11017a24f4d9857e09bcc2445a35794
Dani Tox <DaniTox@users.noreply.github.com>
2018-05-07 23:36:44 +0200

    Initial Commit

---
+++ Tox Home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tox Home
+//
+//  Created by Dani Tox on 07/05/18.
+//  Copyright © 2018 Dani Tox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
